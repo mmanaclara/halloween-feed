d8d64cc2fc7f642d3a8810502786ca4dab2f6404
Clara Moreira <anacacamm@gmail.com>
2022-10-06 11:57:09 -0300

    feat: create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header"
+import './global.css'
+
 export default function App() {
 
   return (
     <>
+      <Header />
       <h1>Helloween</h1>
     </>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+        <strong>halloween feed</strong>
+    </header>
+  )
+}
