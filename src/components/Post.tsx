c9c33089e3d5c6097fe17ec98f1179d139d02405
Clara Moreira <anacacamm@gmail.com>
2022-10-06 15:13:03 -0300

    feat: create comment form

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -25,13 +25,32 @@ export default function Post() {
             <p>Olá, irmãs! 👋</p>
             <p>Vou compartilhar com vocês um feitiço ótimo para rejuvenescer...</p>
 
-            <p>🍵Coloque em um caldeirão: Sangue de coruja com erva vermelha, gire três vezes e acrescente um pelo de sobrancelha, uma pitada de pústula e o dedão de um homem morto (bem fresquinho). Depois, adicione saliva de lagartixa e um pedaço de sua língua.
+            <p>
+                🍵Coloque em um caldeirão: Sangue de coruja com erva vermelha, gire três vezes e acrescente um pelo de sobrancelha, uma pitada de pústula e o dedão de um homem morto (bem fresquinho). Depois, adicione saliva de lagartixa e um pedaço de sua língua.
+            </p>
+            <p>
+                Agora é só dar para a criança beber e correr pro abraço!
             </p>
 
-            <p><a href="#">👉 winifred.witch/feiticosinfaliveis</a></p>
+            <p>Depois comentem o que acharam.</p>
+
+            <p><a href="#">👉 wini.witch/hocuspocus</a></p>
 
             <p><a href="#">#100anosmaisjovem #irmassanderson #abracadabra</a></p>
         </div>
+
+        <form className={styles.commentForm}>
+            <strong>Deixe seu feedback</strong>
+
+            <textarea 
+                placeholder='Deixe um comentário'
+            />
+
+            <footer>
+                <button type="submit">Publicar</button>
+            </footer>
+
+        </form>
 
     </article>
   )
