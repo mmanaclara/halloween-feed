ee60f7de81c0944b00866c646efed40d274f022b
Clara Moreira <anacacamm@gmail.com>
2022-10-06 12:06:38 -0300

    feat: create Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,12 @@
 import styles from './Header.module.css'
 
+import pumpkinIcon from '../assets/pumpkin-icon.png'
+
 export default function Header() {
   return (
     <header className={styles.header}>
-        <strong>halloween feed</strong>
+        <img src={pumpkinIcon} alt="Ícone de uma abóbora" />
+        <strong>Halloween Feed</strong>
     </header>
   )
 }
